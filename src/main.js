815d58db724f787030c35432a345728368ade4c0
Aaron <aaron.schul@web.de>
2020-10-05 15:32:46 +0200

    PWA Config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router';
 import store from './store';
 import Ionic from '@ionic/vue'
 import App from './views/Menu.vue'
+import './registerServiceWorker'
 
 Vue.use(Ionic);
 
