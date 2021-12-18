be1dc1a52303e0a5c77100aa4e6c129a45ca691e
Aaron <aaron.schul@web.de>
2021-12-18 15:20:54 +0100

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 // import VueRouter from 'vue-router'
 // import App from '../views/Menu.vue'
 import MainHome from '../components/Home.vue'
-import MainRLWE from '../components/RLWE.vue'
 import MainTest from '../components/Test.vue'
 import MainKyberInt from '../components/KyberInt.vue'
 import Settings from '../components/Settings.vue'
@@ -33,8 +32,6 @@ const router = new IonicVueRouter({
       meta: { title: 'Kyber Calculator'}
     },
     { path: '/Test', /*name: MainTest,*/ component: MainTest,
-    meta: { title: 'Kyber Calculator'} },
-    { path: '/RLWE', /*name: MainRLWE,*/ component: MainRLWE,
     meta: { title: 'Kyber Calculator'} },
     { path: '/KyberInt', /*name: MainNewHope,*/ component: MainKyberInt,
     meta: { title: 'Kyber Calculator'} },
